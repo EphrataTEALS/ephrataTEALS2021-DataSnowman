d683ad3c9bcec54f43b44bd9aeb8f27287cc1791
DataSnowman <darsch@microsoft.com>
2021-11-07 18:15:42 +0000

    multi

---
+++ sampleCode/Parameters/Multiply.java
@@ -0,0 +1,14 @@
+//Limitations of Parameters
+
+
+public class Multiply {
+    public static void main(String[] args) {
+        int n = 5;
+        doubleNumber(n);
+        System.out.println(n);
+    }
+
+    public static void doubleNumber(int x) {
+        x = x * 2;
+    }
+}
