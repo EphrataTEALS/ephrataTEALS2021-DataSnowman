17dad5be6f57909b46c0736892faffa33bdbf1d8
DataSnowman <darsch@microsoft.com>
2021-11-07 18:23:28 +0000

    multi2

--- sampleCode/Parameters/Multiply.java
+++ sampleCode/Parameters/Multiply.java
@@ -5,10 +5,13 @@ public class Multiply {
     public static void main(String[] args) {
         int n = 5;
         doubleNumber(n);
-        System.out.println(n);
+        System.out.println("This is n value: " + n);
     }
 
     public static void doubleNumber(int x) {
         x = x * 2;
+        System.out.println("This is x value: " + x);
     }
 }
+
+// Need to use a Return Value which you will learn about tomorrow
